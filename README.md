fe23879f345200998490d0524ddd2635225220bc
Chandler Stevens <chandlerstevens22@gmail.com>
2019-09-16 10:45:19 -0700

    Initial commit of v1.3
    Several performance/maintainability improvements and added minor logic.
    Added v1.3
    Update README.md

--- README.md
+++ README.md
@@ -2,39 +2,41 @@
 
 ![alt text](http://img1.starwars-holonet.com/holonet/dictionnaire/photos/droid_rh7.jpg)
 
-## Latest Stable Version: v1.2
+## Latest Stable Version: v1.3
+
+### Version 2: Mulitplayer Edition
++ *Currently in progress (v2.0)*
++ Minimal game logic
++ Console application
++ Minimal input validation and minimal recoverability
++ Supports: Windows (requires Python v3)
+
+### Version 1: Offline Edition
++ **Latest stable version (v1.3)**
++ Basic game logic
++ Console application
++ Excellent input validation and basic recoverability
++ Supports: Windows (requires Python v3) and Android (requires QPython 3)
 
 ## Changelog
 ### v2.0:
-+ *Currently in BETA*
-+ Server and client
++ *Currently in progress*
 + Minimal game logic
-+ Non-graphical
-+ No validation
-+ Requires: Python v3 (Windows)
++ Console application
++ Minimal input validation and minimal recoverability
+
+### v1.3:
++ Enhanced basic game logic
++ Performance/stability/maintainability improvements
 
 ### v1.2:
-+ **Latest stable version**
-+ **Restructured into MVC model**
-+ Client only
-+ Basic game logic
-+ Non-graphical
-+ High validation and robust recovery
-+ Requires: Python v3 (Windows) or QPython 3 (Android)
++ Restructured into MVC model
 
 ### v1.1:
-+ *Made obsolete by v1.2*
-+ Client only
-+ **Added basic game logic**
-+ Non-graphical
-+ **Added high validation and robust recovery**
-+ Requires: Python v3 (Windows) or QPython 3 (Android)
++ Added basic game logic
++ Added excellent input validation and basic recoverability
 
 ### v1.0:
-+ *Made obsolete by v1.1*
-+ Initial version
-+ Client only
 + Minimal game logic
-+ Non-graphical
-+ No validation or recovery
-+ Requires: Python v3 (Windows) or QPython 3 (Android)
++ Console application
++ No validation or recoverability
---
+++ v1/DroidControllerv1u3.py
@@ -0,0 +1,89 @@
+def Validate(num):
+    try:
+        int(str(num).replace(",", ""))
+        return True
+    except ValueError:
+        return False
+
+
+def Request(funds, message):
+    while True:
+        num = input(message)
+        if Validate(num):
+            if int(num) <= 0:
+                print("Invalid Number: Please enter a positive integer.")
+            elif funds != "N/A" and int(num) > funds:
+                print("Insufficient Funds: You only have", funds, "credits.")
+            else:
+                return int(num)
+        elif num == "x":
+            return "x"
+        else:
+            print("Invalid Input: Please enter a positive integer.")
+        print("Type (x) if you want to cancel.")
+
+
+def RequestStart():
+    return Request("N/A", "How many credits are you starting with?\n\t")
+
+def RequestGame(funds):
+    return Request(funds, "How many credits are you using to\n"+
+                          "\tante into the Game Pot?\n\t")
+
+def RequestSabacc(funds):
+   return Request(funds, "How many credits are you using to\n"+
+                                "\tante into the Sabacc Pot?\n\t")
+def RequestOpen(funds):
+    return Request(funds, "How many credits would you like to open with?\n\t")
+
+def RequestCall(funds):
+    return Request(funds, "How many credits would you like to call with?\n\t")
+
+def RequestRaise(funds):
+    return Request(funds, "How many total credits would you like to\n"+
+                          "\traise the highest bet to?\n\t")
+
+def RequestWin():
+    return Request("N/A", "How many more credits did you win?\n\t")
+
+
+def RequestAnte():
+    return (input("\nPlease type the letter of your chosen action:\n"+
+                  "\tAnte/Buy In (a)\n"+
+                  "\tSingle Blind Card Draw Win (w)\n"+
+                  "\tQuit/End Game (q)\n\t")).lower()
+
+def RequestBetting():
+    return (input("\nPlease type the letter of your chosen action:\n"+
+                  "\tPass/Check (p)\n"+
+                  "\tOpen/Bet (o)\n"+
+                  "\tCall/See (c)\n"+
+                  "\tRaise (r)\n"+
+                  "\tFold/Junk (f)\n"+
+                  "\tWin (w)\n"+
+                  "\tQuit/End Game (q)\n\t")).lower()
+
+def RequestBankrupt():
+    return (input("\nPlease type the letter of your chosen action:\n"+
+                  "\tPass/Check (p)\n"+
+                  "\tFold/Junk (f)\n"+
+                  "\tWin (w)\n"+
+                  "\tQuit/End Game (q)\n\t")).lower()
+
+def RequestEnd():
+    input("Game Over.")
+
+
+def DisplaySelection():
+    print("Invalid Selection: Please select an\n"+
+          "\tavailable option.")
+
+def DisplayFunds(funds):
+    if funds != 1:
+        print("You now have", funds, "credits remaining.")
+    else:
+        print("You now have", funds, "credit remaining.")
+
+def DisplayEliminated():
+    print("You have been eliminated from the game and\n"+
+          "\tcleaned out of all your credits!")
